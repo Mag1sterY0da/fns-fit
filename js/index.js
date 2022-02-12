c53890c6da0111354bfc529b6d9cf545e1e71e23
Sander <sashabro2002@gmail.com>
2022-02-12 17:30:22 +0200

    Vers 0.1

--- js/index.js
+++ js/index.js
@@ -1,2 +1,11 @@
-console.log(390/1024*100)
-console.log(556/1440*100)
+const openNav = () => {
+  document.getElementById("mySidenav").style.width = "400px";
+  document.querySelector('.burger-menu-icon').style.display = 'none';
+}
+
+const closeNav = () => {
+  document.getElementById("mySidenav").style.width = "0";
+  setTimeout(() => {
+    document.querySelector('.burger-menu-icon').style.display = 'block';
+  }, 500)
+}
